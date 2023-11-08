da8f9e5b19d389a4045e9ee56bb5dd19b1fc881e
Syed Fareed Ahmed <syedahmed261@gmail.com>
2023-11-09 03:51:33 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure--v8l2(n)@l117izhq(63=dpisy#u$@wn+emjwz)w4&1a)&ou$g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
