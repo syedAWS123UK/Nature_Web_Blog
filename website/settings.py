df284f93c20a6f0836a3cf09ebae4e14960c65fe
Syed Fareed Ahmed <syedahmed261@gmail.com>
2023-11-09 01:28:54 +0530

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--v8l2(n)@l117izhq(63=dpisy#u$@wn+emjwz)w4&1a)&ou$g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
